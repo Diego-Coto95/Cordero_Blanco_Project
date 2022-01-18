149f9d64c2836679b2baeba954898d0a4c695650
Juan Diego Coto <jd506@hotmail.com>
2022-01-18 16:23:29 -0600

    Adding features component, the description is not there because the beneficiary has not provided the documentation

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Navigation } from "./components/navigation";
 import { Header } from "./components/header";
-// import { Features } from "./components/features";
+import { Features } from "./components/features";
 // import { About } from "./components/about";
 // import { Services } from "./components/services";
 // import { Gallery } from "./components/gallery";
@@ -26,9 +26,9 @@ const App = () => {
   return (
     <div>
       <Navigation />
-      { <Header data={landingPageData.Header} />
-      /*<Features data={landingPageData.Features} />
-      <About data={landingPageData.About} />
+      <Header data={landingPageData.Header} />
+      <Features data={landingPageData.Features} />
+      {/* <About data={landingPageData.About} />
       <Services data={landingPageData.Services} />
       <Gallery data={landingPageData.Gallery}/>
       <Testimonials data={landingPageData.Testimonials} />

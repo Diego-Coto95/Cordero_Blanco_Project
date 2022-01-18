788bf11f8792c4effe314c112a7980f8757be1ac
Juan Diego Coto <jd506@hotmail.com>
2022-01-17 18:14:53 -0600

    Adding main image, styles to navbar and animations to submenu. Also i started work in the Feature module

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Navigation } from "./components/navigation";
-// import { Header } from "./components/header";
+import { Header } from "./components/header";
 // import { Features } from "./components/features";
 // import { About } from "./components/about";
 // import { Services } from "./components/services";
@@ -26,8 +26,8 @@ const App = () => {
   return (
     <div>
       <Navigation />
-      {/* <Header data={landingPageData.Header} />
-      <Features data={landingPageData.Features} />
+      { <Header data={landingPageData.Header} />
+      /*<Features data={landingPageData.Features} />
       <About data={landingPageData.About} />
       <Services data={landingPageData.Services} />
       <Gallery data={landingPageData.Gallery}/>
